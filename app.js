cec567643d6bccf8a7b764e8d3651ea313c46eb6
SaraFazliu <67891916+SaraFazliu@users.noreply.github.com>
2021-06-23 13:46:06 +0200

    data fetch function added

---
+++ app.js
@@ -0,0 +1,40 @@
+function jsonDataFetch() {
+    fetch("data.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const node = data
+                .map(cardData => {
+                    let logoPath = "";
+                    if (cardData.source_type==="instagram")
+                        logoPath = "icons/instagram-logo.svg";
+                    else
+                        logoPath = "icons/facebook.svg";
+
+                    const date = new Date(cardData.date);
+                    let dtf = date.toLocaleDateString('en-GB', {
+                        day: 'numeric', month: 'short', year: 'numeric'
+                    })
+
+                    return `<div class="card" name="card" style="display: none" onclick="showCard(this)">
+                                <div class="profile-content">
+                                    <div class="profile-image"><img src="${cardData.profile_image}"></div> 
+                                    <div class="profile-info"><div class="name">${cardData.name}</div><div class="date">${dtf}</div></div>
+                                    <div class="logo"><img src="${logoPath}"></div>
+                                </div>
+                                <div class="card-post"><img src="${cardData.image}"></div>
+                                <div class="card-caption">${cardData.caption}</div>
+                                <hr>
+                                <div class="card-likes"><img src="icons/heart.svg"><span class="likes"> ${cardData.likes}</span></div>
+                            </div>
+                            `;
+                }).join("");
+
+
+            document.querySelector(".cards").insertAdjacentHTML("beforeend", node);
+
+        })
+        .then(loadCards)
+}
+jsonDataFetch();
